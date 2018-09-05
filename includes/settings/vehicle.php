12e8b75a0a718a7b53cca5bb1da9f9187f37f6d1
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-09-05 12:17:14 -0300

    Fixing spaces to trigger travis

--- includes/settings/vehicle.php
+++ includes/settings/vehicle.php
@@ -1,28 +1,28 @@
 <div class="uix-field-wrapper">
 
 	<ul class="ui-tab-nav">
-		<li><a href="#ui-general" class="active"><?php esc_html_e('General','tour-operator'); ?></a></li>
-		<?php if(class_exists('LSX_TO_Search')) { ?>
-			<li><a href="#ui-search"><?php esc_html_e('Search','tour-operator'); ?></a></li>
+		<li><a href="#ui-general" class="active"><?php esc_html_e( 'General', 'tour-operator' ); ?></a></li>
+		<?php if ( class_exists( 'LSX_TO_Search' ) ) { ?>
+			<li><a href="#ui-search"><?php esc_html_e( 'Search', 'tour-operator' ); ?></a></li>
 		<?php } ?>
-		<li><a href="#ui-placeholders"><?php esc_html_e('Placeholders','tour-operator'); ?></a></li>
-		<li><a href="#ui-archives"><?php esc_html_e('Archives','tour-operator'); ?></a></li>
-		<li><a href="#ui-single"><?php esc_html_e('Single','tour-operator'); ?></a></li>
+		<li><a href="#ui-placeholders"><?php esc_html_e( 'Placeholders', 'tour-operator' ); ?></a></li>
+		<li><a href="#ui-archives"><?php esc_html_e( 'Archives', 'tour-operator' ); ?></a></li>
+		<li><a href="#ui-single"><?php esc_html_e( 'Single', 'tour-operator' ); ?></a></li>
 	</ul>
 
 	<div id="ui-general" class="ui-tab active">
 		<table class="form-table">
 			<tbody>
-			<?php do_action('lsx_to_framework_vehicle_tab_content','vehicle','general'); ?>
+			<?php do_action( 'lsx_to_framework_vehicle_tab_content', 'vehicle', 'general' ); ?>
 			</tbody>
 		</table>
 	</div>
 
-	<?php if(class_exists('LSX_TO_Search')) { ?>
+	<?php if ( class_exists( 'LSX_TO_Search' ) ) { ?>
 		<div id="ui-search" class="ui-tab">
 			<table class="form-table">
 				<tbody>
-				<?php do_action('lsx_to_framework_vehicle_tab_content','vehicle','search'); ?>
+				<?php do_action( 'lsx_to_framework_vehicle_tab_content', 'vehicle', 'search' ); ?>
 				</tbody>
 			</table>
 		</div>
@@ -31,7 +31,7 @@
 	<div id="ui-placeholders" class="ui-tab">
 		<table class="form-table">
 			<tbody>
-			<?php do_action('lsx_to_framework_vehicle_tab_content','vehicle','placeholders'); ?>
+			<?php do_action( 'lsx_to_framework_vehicle_tab_content', 'vehicle', 'placeholders' ); ?>
 			</tbody>
 		</table>
 	</div>
@@ -39,7 +39,7 @@
 	<div id="ui-archives" class="ui-tab">
 		<table class="form-table">
 			<tbody>
-			<?php do_action('lsx_to_framework_vehicle_tab_content','vehicle','archives'); ?>
+			<?php do_action( 'lsx_to_framework_vehicle_tab_content', 'vehicle', 'archives' ); ?>
 			</tbody>
 		</table>
 	</div>
@@ -47,9 +47,9 @@
 	<div id="ui-single" class="ui-tab">
 		<table class="form-table">
 			<tbody>
-			<?php do_action('lsx_to_framework_vehicle_tab_content','vehicle','single'); ?>
+			<?php do_action( 'lsx_to_framework_vehicle_tab_content', 'vehicle', 'single' ); ?>
 			</tbody>
 		</table>
 	</div>
-	<?php do_action('lsx_to_framework_vehicle_tab_bottom','vehicle'); ?>
+	<?php do_action( 'lsx_to_framework_vehicle_tab_bottom', 'vehicle' ); ?>
 </div>
