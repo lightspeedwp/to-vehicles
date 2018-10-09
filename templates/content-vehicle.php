25293c42c3053b02c7b9ac6ad11ca23b4b3aa328
Warwick Booth <warwick@lsdev.biz>
2018-10-09 18:20:39 +0200

    Fixed

--- templates/content-vehicle.php
+++ templates/content-vehicle.php
@@ -81,18 +81,16 @@ if ( 1 !== $lsx_archive ) {
 								</div>
 							</section>
 							<?php if ( lsx_to_has_enquiry_contact() ) : ?>
-								<div class="col-xs-12 col-sm-12 col-md-6">
-									<div class="lsx-to-contact-widget">
-										<?php
-										if ( function_exists( 'lsx_to_has_team_member' ) && lsx_to_has_team_member() ) {
-											lsx_to_team_member_panel( '<div class="lsx-to-contact">', '</div>' );
-										} else {
-											lsx_to_enquiry_contact( '<div class="lsx-to-contact">', '</div>' );
-										}
+								<div class="lsx-to-contact-widget">
+									<?php
+									if ( function_exists( 'lsx_to_has_team_member' ) && lsx_to_has_team_member() ) {
+										lsx_to_team_member_panel( '<div class="lsx-to-contact">', '</div>' );
+									} else {
+										lsx_to_enquiry_contact( '<div class="lsx-to-contact">', '</div>' );
+									}
 
-										lsx_to_enquire_modal();
-										?>
-									</div>
+									lsx_to_enquire_modal();
+									?>
 								</div>
 							<?php endif ?>
 						</div>
